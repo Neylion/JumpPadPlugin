1110f4d7fef4ab5ba489f8a9580880d80b5a1afa
vanbrayne <lars.lindberg@xlent.se>
2014-09-23 21:46:05 +0200

    Compiler warnings.

--- src/main/java/se/fredsfursten/jumppadplugin/Jumper.java
+++ src/main/java/se/fredsfursten/jumppadplugin/Jumper.java
@@ -1,16 +1,12 @@
 package se.fredsfursten.jumppadplugin;
 
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.HashMap;
 
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
-import org.bukkit.util.Vector;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 public class Jumper implements Listener {
 	private static Jumper singleton = null;
